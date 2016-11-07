83d18b52f67b35a3c0e56c6f230678d4e120c664
Ranju <ranjukr@thoughtworks.com>
2016-11-07 15:46:42 +0530

    @ranjukr: assignment-4: added value of n and square of n.

--- oneToTen/js/visualize.js
+++ oneToTen/js/visualize.js
@@ -10,36 +10,52 @@ var numbers = function () {
 }();
 
 var getSqure = function () {
-    var allNumber = numbers.map(function (value) {
-        return value*value;
+    return numbers.map(function (value) {
+        return value * value;
     });
-    return allNumber;
 };
 
 var getLog = function () {
-    var allNumber = numbers.map(function (value) {
+    return numbers.map(function (value) {
         return Math.log(value).toFixed(3);
     });
-    return allNumber;
 };
 
 
 var getLogRound = function () {
-    var allNumber = numbers.map(function (value) {
+    return numbers.map(function (value) {
         return Math.round(Math.log10(value));
     });
-    return allNumber;
 };
 
 var visualize = function() {
     var title = d3.select('.title-container').append('div').attr('class', 'titles');
+    var n_number = d3.select('.number-container').append('div').attr('class', 'numbers');
+    var n_square = d3.select('.n-square-container').append('div').attr('class', 'n-squares');
+
     var titles = title.selectAll('.titles').data(numbers);
+    var n_numbers = n_number.selectAll('.numbers').data(numbers);
+    var n_squares = n_square.selectAll('.n-squares').data(getSqure());
 
     titles.enter()
         .append("div")
         .attr("class", "title");
 
     d3.selectAll('.title')
+        .text(function (d) {return d});
+
+    n_numbers.enter()
+        .append("div")
+        .attr("class", "number");
+
+    d3.selectAll('.number')
+        .text(function (d) {return d});
+
+    n_squares.enter()
+        .append("div")
+        .attr("class", "n-square");
+
+    d3.selectAll('.n-square')
         .text(function (d) {return d});
 };
 
