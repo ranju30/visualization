6826832081d5c1403cf149fa5e9809dd5ebe7181
Ranju <ranjukr@thoughtworks.com>
2016-11-07 20:46:20 +0530

    @ranjukr: assignment-4: done according to the requirement.

--- oneToTen/js/visualize.js
+++ oneToTen/js/visualize.js
@@ -1,62 +1,34 @@
-const MIN_VALUE = 1;
-const MAX_VALUE = 10;
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var numbers = function () {
-    var number = [];
-    for (var i=MIN_VALUE;i<=MAX_VALUE;i++){
-        number.push(i);
-    }
-    return number;
-}();
-
-var getSqure = function () {
-    return numbers.map(function (value) {
-        return value * value;
-    });
+var getRow = function(scale, name) {
+    var tableRow = d3.select('tbody').append('tr');
+    tableRow.append('td').text(name);
+    tableRow.selectAll('td')
+        .data(numbers, function(d) {
+            return d;
+        })
+        .enter()
+        .append('td')
+        .text(function(d) {
+            return scale(d);
+        });
 };
 
-var getLog = function () {
-    return numbers.map(function (value) {
-        return Math.log(value).toFixed(3);
-    });
+var linearScale = d3.scaleLinear();
+var square = d3.scalePow().exponent(2);
+var log = function(d) {
+    return d3.scaleLog()(d).toFixed(4);
+};
+var logRounded = d3.scaleLog().rangeRound([0, 1]);
+
+var loadChart = function() {
+    var table = d3.select('body').append('table');
+    table.append('tbody');
+    getRow(linearScale, 'Title');
+    getRow(linearScale, 'n');
+    getRow(square, 'n square');
+    getRow(log, 'log(n)');
+    getRow(logRounded, 'log(n) rounded');
 };
 
-
-var getLogRound = function () {
-    return numbers.map(function (value) {
-        return Math.round(Math.log10(value));
-    });
-};
-
-var visualize = function() {
-    var title = d3.select('.title-container').append('div').attr('class', 'titles');
-    var n_number = d3.select('.number-container').append('div').attr('class', 'numbers');
-    var n_square = d3.select('.n-square-container').append('div').attr('class', 'n-squares');
-
-    var titles = title.selectAll('.titles').data(numbers);
-    var n_numbers = n_number.selectAll('.numbers').data(numbers);
-    var n_squares = n_square.selectAll('.n-squares').data(getSqure());
-
-    titles.enter()
-        .append("div")
-        .attr("class", "title");
-
-    d3.selectAll('.title')
-        .text(function (d) {return d});
-
-    n_numbers.enter()
-        .append("div")
-        .attr("class", "number");
-
-    d3.selectAll('.number')
-        .text(function (d) {return d});
-
-    n_squares.enter()
-        .append("div")
-        .attr("class", "n-square");
-
-    d3.selectAll('.n-square')
-        .text(function (d) {return d});
-};
-
-window.onload = visualize;
+window.onload = loadChart;
