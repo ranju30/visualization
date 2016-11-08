8250c275a1dfb288ed42b6687377033707043e13
Ranju <ranjukr@thoughtworks.com>
2016-11-08 12:54:55 +0530

    @ranjukr: assignment-5: implemented to show the data according to the requirement.

--- oneToTen/js/borderNumber.js
+++ oneToTen/js/borderNumber.js
@@ -1,19 +1,24 @@
 var numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 var loadChart = function(){
-  var table = d3.select('body').append('table');
-  table.append('tbody');
-  var tableRow = d3.select('tbody').append('tr');
-  tableRow.selectAll('td')
+
+var scale = d3.scaleLinear()
+              .domain([0,10])
+              .range(['italic bold 12px/30px Georgia,sans-serif', 'italic bold 120px/180px Georgia,sans-serif']);
+
+  var table = d3.select('body').append('div');
+  var tableRow = d3.select('div').append('div').attr('class','tableDiv');
+  tableRow.selectAll('.tableDiv')
       .data(numbers, function(d) {
           return d;
       })
       .enter()
-      .append('td')
+      .append('div')
       .text(function(d) {
           return d;
       })
-      .style('font-size',function(d,i){return 10.91*(i+1)+'px';});
+      .attr('class','number')
+      .style('font',function(d){return scale(d) + 'px';});
 
 };
 
