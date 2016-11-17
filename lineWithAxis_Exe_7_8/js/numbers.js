75b0bf8be5448bf4b02349f969618d95ce750ff9
Ranju <ranjukr@thoughtworks.com>
2016-11-17 11:54:25 +0530

    @ranjukr: assignment-7: Added line according to the data.

--- lineWithAxis_Exe_7_8/js/numbers.js
+++ lineWithAxis_Exe_7_8/js/numbers.js
@@ -1,22 +1,18 @@
 const WIDTH = 1280;
-const HEIGHT = 800;
+const HEIGHT = 600;
 const MARGIN = 30;
 
 const SCALERANGE = 500;
 
-var pointValues = [
-                   {x: 0, y: 5}, {x: 1, y: 9}, {x: 2, y: 7},
-                   {x: 3, y: 5}, {x: 4, y: 3}, {x: 6, y: 4},
-                   {x: 7, y: 2}, {x: 8, y: 3}, {x: 9, y: 2}
-                 ];
+var data = [{x: 0, y: 5}, {x: 1, y: 9}, {x: 2, y: 7},{x: 3, y: 5}, {x: 4, y: 3}, {x: 6, y: 4},{x: 7, y: 2}, {x: 8, y: 3}, {x: 9, y: 2}];
 
 var xScale = d3.scaleLinear()
     .domain([0 / 10, 10 / 10])
-    .range([1, SCALERANGE]);
+    .range([0, SCALERANGE]);
 
 var yScale = d3.scaleLinear()
-    .domain([10 / 10, 0 / 10])
-    .range([1, SCALERANGE]);
+    .domain([0 / 10, 10 / 10])
+    .range([SCALERANGE, 0]);
 
 var xAxis = d3.axisBottom(xScale);
 var yAxis = d3.axisLeft(yScale);
@@ -25,18 +21,31 @@ var translate = function(x, y) {
     return 'translate(' + x + ',' + y + ')';
 }
 
+var line = d3.line()
+.x(function(d){return xScale(d.x/10)})
+.y(function(d){return yScale(d.y/10)})
+
 var visualize = function() {
     var svg = d3.select('.container').append('svg')
-        .attr('height', HEIGHT + MARGIN)
-        .attr('width', WIDTH + MARGIN);
+        .attr('height', HEIGHT)
+        .attr('width', WIDTH);
 
     svg.append('g')
         .attr('transform', translate(MARGIN, SCALERANGE + MARGIN))
         .call(xAxis);
 
     svg.append('g')
-        .attr('transform', translate(MARGIN + 1, MARGIN))
+        .attr('transform', translate(MARGIN , MARGIN))
         .call(yAxis);
+
+    var g = svg.append('g')
+        .attr('transform', translate(MARGIN, MARGIN));
+
+        g.append("path")
+        .attr('class','line')
+        .attr("d", line(data));
+
+
 };
 
 window.onload = visualize;
