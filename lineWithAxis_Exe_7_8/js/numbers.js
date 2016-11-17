a176d9913d07f455d44c6fd3eb07a6b08ff45825
Ranju <ranjukr@thoughtworks.com>
2016-11-17 12:53:58 +0530

    @ranjukr: assignment-7: Added Sin line with the value of 0 to 10.

--- lineWithAxis_Exe_7_8/js/numbers.js
+++ lineWithAxis_Exe_7_8/js/numbers.js
@@ -6,12 +6,20 @@ const SCALERANGE = 500;
 
 var data = [{x: 0, y: 5}, {x: 1, y: 9}, {x: 2, y: 7},{x: 3, y: 5}, {x: 4, y: 3}, {x: 6, y: 4},{x: 7, y: 2}, {x: 8, y: 3}, {x: 9, y: 2}];
 
+var getSinValue = function(limit){
+  var storage = [];
+  for(var index = 0; index<limit; index++){
+    storage.push(Math.sin(index));
+  }
+    return storage;
+}
+
 var xScale = d3.scaleLinear()
-    .domain([0 / 10, 10 / 10])
+    .domain([0,1])
     .range([0, SCALERANGE]);
 
 var yScale = d3.scaleLinear()
-    .domain([0 / 10, 10 / 10])
+    .domain([0, 1])
     .range([SCALERANGE, 0]);
 
 var xAxis = d3.axisBottom(xScale);
@@ -22,8 +30,20 @@ var translate = function(x, y) {
 }
 
 var line = d3.line()
-.x(function(d){return xScale(d.x/10)})
-.y(function(d){return yScale(d.y/10)})
+    .x(function(d) {
+        return xScale(d.x/10)
+    })
+    .y(function(d) {
+        return yScale(d.y/10)
+    });
+
+var sinLine = d3.line()
+    .x(function(d,i) {
+        return xScale(i/10);
+    })
+    .y(function(d, i) {
+        return yScale(d/10 + 0.5);
+    })
 
 var visualize = function() {
     var svg = d3.select('.container').append('svg')
@@ -35,17 +55,22 @@ var visualize = function() {
         .call(xAxis);
 
     svg.append('g')
-        .attr('transform', translate(MARGIN , MARGIN))
+        .attr('transform', translate(MARGIN, MARGIN))
         .call(yAxis);
 
-    var g = svg.append('g')
+    var gLine = svg.append('g')
         .attr('transform', translate(MARGIN, MARGIN));
 
-        g.append("path")
-        .attr('class','line')
+    gLine.append("path")
+        .attr('class', 'line')
         .attr("d", line(data));
 
+    var gSin = svg.append('g')
+        .attr('transform', translate(MARGIN, MARGIN));
 
+    gSin.append("path")
+        .attr('class', 'line')
+        .attr("d", sinLine(getSinValue(10)));
 };
 
 window.onload = visualize;
