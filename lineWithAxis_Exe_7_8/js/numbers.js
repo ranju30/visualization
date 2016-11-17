7a71b5465d15a308056955fc60f6c1acefcfd404
Ranju <ranjukr@thoughtworks.com>
2016-11-17 13:05:18 +0530

    @ranjukr: assignment-7: refactored to make indentation properly.

--- lineWithAxis_Exe_7_8/js/numbers.js
+++ lineWithAxis_Exe_7_8/js/numbers.js
@@ -6,16 +6,16 @@ const SCALERANGE = 500;
 
 var data = [{x: 0, y: 5}, {x: 1, y: 9}, {x: 2, y: 7},{x: 3, y: 5}, {x: 4, y: 3}, {x: 6, y: 4},{x: 7, y: 2}, {x: 8, y: 3}, {x: 9, y: 2}];
 
-var getSinValue = function(limit){
-  var storage = [];
-  for(var index = 0; index<limit; index++){
-    storage.push(Math.sin(index));
-  }
+var getSinValue = function(limit) {
+    var storage = [];
+    for (var index = 0; index < limit; index++) {
+        storage.push(Math.sin(index));
+    }
     return storage;
 }
 
 var xScale = d3.scaleLinear()
-    .domain([0,1])
+    .domain([0, 1])
     .range([0, SCALERANGE]);
 
 var yScale = d3.scaleLinear()
@@ -31,18 +31,18 @@ var translate = function(x, y) {
 
 var line = d3.line()
     .x(function(d) {
-        return xScale(d.x/10)
+        return xScale(d.x / 10)
     })
     .y(function(d) {
-        return yScale(d.y/10)
+        return yScale(d.y / 10)
     });
 
 var sinLine = d3.line()
-    .x(function(d,i) {
-        return xScale(i/10);
+    .x(function(d, i) {
+        return xScale(i / 10);
     })
     .y(function(d, i) {
-        return yScale(d/10 + 0.5);
+        return yScale(d / 10 + 0.5);
     })
 
 var visualize = function() {
